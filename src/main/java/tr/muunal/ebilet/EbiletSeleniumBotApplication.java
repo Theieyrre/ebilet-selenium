ee6c29c8b8e6915e84a95d9f16bed5304ef84f62
Mustafa Ünal <crizfmtfm@gmail.com>
2024-11-19 09:53:41 +0300

    Init commit

---
+++ src/main/java/tr/muunal/ebilet/EbiletSeleniumBotApplication.java
@@ -0,0 +1,13 @@
+package tr.muunal.ebilet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbiletSeleniumBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbiletSeleniumBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/muunal/ebilet/EbiletSeleniumBotApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.muunal.ebilet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbiletSeleniumBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
